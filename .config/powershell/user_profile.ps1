9faf04efdc9353636cfefcad9226e7d58d51050c
Canela-san <sstarksan@gmail.com>
2022-02-13 13:05:37 -0300

    Sem descrição

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -27,7 +27,8 @@ Set-Alias -Name vim -Value nvim
 Set-Alias ll ls
 Set-Alias g git
 Set-Alias grep findstr
-Set-Alias win 'C:\Users\Canela\Geral\Win.ps1'
+Set-Alias win 'C:\Users\Canela\Geral\win.ps1'
+Set-Alias update 'C:\Users\Canela\Geral\update.ps1'
 Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 
