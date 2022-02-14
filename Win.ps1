cf756628724ce9dbab376652cb929a37caee97cf
Canela-san <sstarksan@gmail.com>
2022-02-14 12:14:05 -0300

    Sem descrição

--- Win.ps1
+++ Win.ps1
@@ -18,7 +18,7 @@ while($Temp_menu)
 	write-host "`n`n ---------------------- MENU ----------------------"
 	write-host " "
 	write-host "  [0] - Sair"
-	write-host "  [1] - All {2,3,4}"
+	write-host "  [1] - All (2,3,4,5)"
 	write-host "  [2] - Desfragmentar"
 	write-host "  [3] - Corrigir integridade da imagem do windows"
 	write-host "  [4] - Corrigir integridade dos arquivos do Windows"
@@ -34,19 +34,45 @@ while($Temp_menu)
 	{
 		Exit
 	}
+
+	if ($menu -in $defrag)
+	{
+		$temp_menu_defrag = $true
+		while($temp_menu_defrag)
+		{
+			Clear-Host
+        		write-host "`n`nO diretório C: é um HD ou SSD?"
+			write-host " [1] HD (Hard Disk)"
+			write-host " [2] SSD`n"
+			write-host " [0] Cancelar"
+			
+			$TypeDefrag = Read-Host -Prompt '-> '
+                        
+			if ($TypeDefrag -in 1,2)
+                        {
+                                $Temp_menu_defrag = $false
+                        }
+                        elseif ($TypeDefrag -eq 0)
+                        {
+                                $menu = ""
+                                $Temp_menu_defrag = $false
+                        }
+		}
+	}
+
 	if ($menu -in $WindowsDefender)
-    {
-    	$Temp_menu_defender = $true
+   	{
+    		$Temp_menu_defender = $true
 		while($Temp_menu_defender)
 		{
 			Clear-Host
-			write-host "`nQue tipo de verificação você deseja?"
+			write-host "`n`nQue tipo de verificação você deseja?"
 			write-host " [1] Verificação Rápida"
 			write-host " [2] Verificação Completa`n"
 			write-host " [0] Cancelar"
-            $TypeDefender = Read-Host -Prompt '-> '
-            if ($TypeDefender -in 1,2)
-        	{
+            		$TypeDefender = Read-Host -Prompt '-> '
+            		if ($TypeDefender -in 1,2)
+        		{
 				$Temp_menu_defender = $false
 			}
 			elseif ($TypeDefender -eq 0)
@@ -61,12 +87,13 @@ while($Temp_menu)
 		if ($menu -in $quant_menu)
 		{
 			$Temp_menu = $false
-			write-host "`nDeseja reiniciar o computador no final da execução? (Y) ou (N)"
-            $R_temp = Read-Host -Prompt '-> '
+			Clear-Host
+			write-host "`n`nDeseja reiniciar o computador no final da execução? (Y) ou (N)"
+            		$R_temp = Read-Host -Prompt '-> '
 			if (($R_temp -eq 'Y') -or ($R_temp -eq 'y'))
-            {
+            		{
 				$reiniciar = $true
-            }
+            		}
 			elseif (($R_temp -eq 'N') -or ($R_temp -eq 'n'))
 			{
 				$reiniciar = $false
@@ -74,8 +101,8 @@ while($Temp_menu)
 		}
 		else
 		{
-            $Temp_menu = $true
-        }
+            		$Temp_menu = $true
+        	}
 	}
 }
 
@@ -84,11 +111,22 @@ Switch ($menu){
 	#Desfragmentação (1, 2)
 	{$PSItem -in $defrag}
 	{
-		Clear-Host
-		write-host "`n`n`n - - - - Desfragmentação do Disco C: - - - -`n"
-		write-host " processando...`n"
-		defrag C: -W -F
-		write-host "`n Desfragmentação concluida!"
+		if ($TypeDefrag -eq 1)
+		{
+			Clear-Host
+			write-host "`n`n`n - - - - Desfragmentação do Disco C: - - - -`n"
+			write-host " processando...`n"
+			defrag C: -W -F
+			write-host "`n Desfragmentação concluida!"
+		}
+		else
+		{
+			Clear-Host
+			write-host "`n`n`nDesfragmentação não será executada pois C: é um SSD."
+			write-host "É contra indicativo desfragmentar SSDs, é inutil e degrada a vida útil da peça."
+			write-host "Desfragmentar é indicado apenas em HDs (Hard Disks)."
+			write-host "`nA desfragmentação será pulada da execução."
+		}
 		timeout /t 5 /nobreak
 	}
 
