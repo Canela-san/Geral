03c7e1a7dd57d12413bd1aeca38a7baa7178fbb5
Canela-san <sstarksan@gmail.com>
2022-06-09 17:08:09 -0300

    Sem descrição

--- Modules/Canela/Git-Update.ps1
+++ Modules/Canela/Git-Update.ps1
@@ -1,17 +1,14 @@
 Set-Alias update Git-Update
 Function Git-Update {
 $CurrentPath = pwd
-$CurrentPath
-Write-Host "Caminho é $CurrentPath"
 Set-Location $PSScriptRoot
-Write-Host "Caminho é $PSScriptRoot"
-$CurrentPath = pwd
-$CurrentPath
-write-host "------- Aaaaatualizando a pasta .config ------- `n"
+<#
+write-host "------- Atualizando a pasta .config ------- `n"
 $SourcePath = 'C:\Users\Canela\.config'
 $DestinationPath = 'C:\Users\Canela\Geral\.config'
 robocopy $SourcePath $DestinationPath /E
 write-host "`n Atualizado com sucesso `n`n"
+#>
 write-host " ------ Atualizando Repositorio Github ------ `n`n"
 write-host " - git add . `n"
 git add .
@@ -20,5 +17,6 @@ git commit -m "Sem descrição"
 write-host "`n - git push -u origin main `n"
 git push -u origin main
 write-host ""
+Set-Location $CurrentPath
 pause
 }
