459a459ac27a135a575a9926f8166f38574ca564
Canela-san <sstarksan@gmail.com>
2022-06-09 16:47:34 -0300

    Sem descrição

--- Modules/Canela/Git-Update.ps1
+++ Modules/Canela/Git-Update.ps1
@@ -1,3 +1,5 @@
+Set-Alias update Git-Update
+Function Git-Update {
 cls
 write-host "------- Atualizando a pasta .config ------- `n"
 $SourcePath = 'C:\Users\Canela\.config'
@@ -7,13 +9,10 @@ write-host "`n Atualizado com sucesso `n`n"
 write-host " ------ Atualizando Repositorio Github ------ `n`n"
 write-host " - git add . `n"
 git add .
-write-host ""
-write-host " - git commit -m 'Sem descrição' "
-write-host ""
+write-host "`n - git commit -m 'Sem descrição' `n"
 git commit -m "Sem descrição"
-write-host ""
-write-host " - git push -u origin main "
-write-host ""
+write-host "`n - git push -u origin main `n"
 git push -u origin main
 write-host ""
 pause
+}
