82e05dc5472f71ab1740ce15ec82af4fc81ada71
Canela-san <sstarksan@gmail.com>
2022-06-09 20:34:13 -0300

    Sem descrição

--- Setup.ps1
+++ Setup.ps1
@@ -1,3 +1,4 @@
+Set-Alias setup 'C:\Users\Canela\Geral\Setup.ps1'
 $Module = $PSScriptRoot + '\Modules'
 (Get-ChildItem -Path $Module -Include *.psm1 -Recurse).Fullname | % { Import-Module $_ -DisableNameChecking -Force }
 
